5aea23c59c233a525144ed190e904a6f58e53983
Gabriel Duran <gabodescansando999@gmail.com>
2024-06-26 14:06:17 -0400

    Primera version

---
+++ Calculadora_v1.py
@@ -0,0 +1,12 @@
+print("bienvenido a la calculadora de promedios de notas")
+a = float(input("Ingrese su primera nota :"))
+b = float(input("Ingrese su segunda nota :"))
+c = float(input("Ingrese su tercera nota :"))
+d = float(input("Ingrese su cuarta nota :"))
+e = float(input("Ingrese su quinta nota :"))
+
+suma = a+b+c+d+e
+promedio = suma/5
+
+print(f"El promedio de nota es : {promedio}")
+
